263895328060ae0d9859ff6fc561fa67f0297bf0
kiet.vpt <kiet.vpt@gmail.com>
2024-09-05 18:15:39 +0700

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': [ // Thêm rule của Prettier
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ],
+    },
+    ignorePatterns: [ // Thêm ignorePatterns để không kiểm tra vite.config.ts
+      'vite.config.ts'
+    ],
+    extends: [ // Thêm eslint-config-prettier và prettier vào extends
+      'eslint-config-prettier',
+      'prettier'
+    ],
+  },
+]
---
+++ src/components/BackgroundBase.tsx
@@ -0,0 +1,9 @@
+import imageRight from "../assets/right-financial.svg"
+
+export function BackgroundBase() {
+    return(
+        <div >
+            <img src={imageRight} alt="right-financial" className="absolute bottom-0 right-0 w-[30rem]"/>
+        </div>
+    )
+}
